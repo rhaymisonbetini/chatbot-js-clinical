ee7986d7cfe8d099f45aaeb967a745468bdba6e5
RHAYMISON <rhaymison@hotmail.com>
2021-05-17 08:17:46 -0300

    att

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ let db = null;
 const url = 'mongodb://localhost:27017';
 const dbName = 'chatbot';
 const tableName = 'chatbot';
+const userTableName = 'usuarios';
 
 const jsonPaser = bodyPaser.json();
 const urlencondedPaser = bodyPaser.urlencoded({ extended: false });
@@ -25,30 +26,45 @@ MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (_
 app.listen(3000)
 console.log('Servidor rodando na porta 3000.')
 
-//routes post
+//routes  de usuario
+app.post('/user/insert', urlencondedPaser, async (req, res) => {
+    let objJSON = await mountCreateUse(req);
+    insertUser(objJSON, (result) => {
+        res.send(result);
+    })
+})
 
-app.post('/question', urlencondedPaser, async (req, res) => {
+app.post('/chatbot/update', urlencondedPaser, async (req, res) => {
+    let objJSON = await mountUpdateUser(req);
+    updateUser(objJSON, (result) => {
+        res.send(result);
+    })
+})
+
+
+//routes chatbot
+app.post('/chatbot/question', urlencondedPaser, async (req, res) => {
     let objJSON = await auxQuestion(req);
     questionData(objJSON, (result) => {
         res.send(result);
     })
 })
 
-app.post('/insert', urlencondedPaser, async (req, res) => {
+app.post('/chatbot/insert', urlencondedPaser, async (req, res) => {
     let objJSON = await mountInsertObject(req);
     insertData(objJSON, (result) => {
         res.send(result);
     })
 })
 
-app.post('/update', urlencondedPaser, async (req, res) => {
+app.post('/chatbot/update', urlencondedPaser, async (req, res) => {
     let objJSON = await mountJsonObjectUpdateDelete(req);
     updateData(objJSON, (result) => {
         res.send(result);
     })
 })
 
-app.post('/delete', urlencondedPaser, async (req, res) => {
+app.post('/chatbot/delete', urlencondedPaser, async (req, res) => {
     let objJSON = await mountJsonObjectUpdateDelete(req);
     deleteData(objJSON, (result) => {
         res.send(result);
@@ -56,14 +72,34 @@ app.post('/delete', urlencondedPaser, async (req, res) => {
 })
 
 
-app.post('/find', urlencondedPaser, async (req, res) => {
+app.post('/chatbot/find', urlencondedPaser, async (req, res) => {
     let objJSON = await mountJsonObjectUpdateDelete(req);
     findData(objJSON, (result) => {
         res.send(result);
     })
 })
 
-//metodos
+//metodos de chatbot
+const insertUser = (objJSON, callback) => {
+    const collection = db.collection(userTableName);
+    collection.insertOne(objJSON, (_, result) => {
+        assert.equal(null, _);
+        callback(result);
+    });
+}
+
+const updateUser = (objJSON, callback) => {
+    const collection = db.collection(userTableName);
+    const code_user = objJSON.code_user;
+    collection.updateUser({ code_user: code_user }, { $set: objJSON }, (_, result) => {
+        assert.equal(null, _);
+        callback(result);
+    });
+}
+
+
+
+//metodos de chatbot
 const questionData = async (objJSON, callback) => {
     const collection = db.collection(tableName);
     collection.find(objJSON).toArray((_, result) => {
@@ -76,9 +112,20 @@ const questionData = async (objJSON, callback) => {
 
             collection.find(objFind).toArray(async (_, result) => {
                 assert.equal(null, _);
-                let nlp = new Nlp();
-                let possibleResult = await nlp.NatualLanguageProcess(objJSON.input, result);
-                callback(possibleResult);
+
+                if (result.length <= 0) {
+
+                    collection.find({ code_user: objJSON.code_user }).toArray(async (_, result) => {
+                        let nlp = new Nlp();
+                        let possibleResult = await nlp.NatualLanguageProcess(objJSON.input, result);
+                        callback(possibleResult);
+                    })
+
+                } else {
+                    let nlp = new Nlp();
+                    let possibleResult = await nlp.NatualLanguageProcess(objJSON.input, result);
+                    callback(possibleResult);
+                }
             })
         } else {
             callback(result)
@@ -123,9 +170,29 @@ const findData = (objJSON, callback) => {
 async function auxQuestion(req) {
     let objJSON = {};
     req.body.code_user ? objJSON.code_user = Number(req.body.code_user) : objJSON.code_user = 0;
-    req.body.code_session ? objJSON.code_session = Number(req.body.code_session) : objJSON.code_session = 0;
+    req.body.activate ? objJSON.activate = req.body.activate : objJSON.activate = 1;
+    req.body.full_name ? objJSON.full_name = req.body.full_name : objJSON.full_name = '';
     req.body.code_before ? objJSON.code_before = Number(req.body.code_before) : objJSON.code_before = 0;
     req.body.input ? objJSON.input = req.body.input : objJSON.input = '';
+    return objJSON;
+}
+
+async function mountCreateUse(req) {
+    let objJSON = {};
+    req.body.code_user ? objJSON.code_user = req.body.code_user : objJSON.code_user = await cod();
+    req.body.activate ? objJSON.activate = req.body.activate : objJSON.activate = 1;
+    req.body.full_name ? objJSON.full_name = req.body.full_name : objJSON.full_name = '';
+    req.body.email ? objJSON.email = req.body.email : objJSON.email = '';
+    req.body.password ? objJSON.password = req.body.password : objJSON.password = '';
+    return objJSON;
+}
+
+async function mountUpdateUser(req) {
+    let objJSON = {};
+    req.body.activate ? objJSON.activate = req.body.activate : objJSON.activate = 1;
+    req.body.full_name ? objJSON.full_name = req.body.full_name : objJSON.full_name = '';
+    req.body.email ? objJSON.email = req.body.email : objJSON.email = '';
+    req.body.password ? objJSON.password = req.body.password : objJSON.password = '';
     return objJSON;
 }
 
@@ -133,7 +200,8 @@ async function auxQuestion(req) {
 async function mountInsertObject(req) {
     let objJSON = {};
     req.body.code_user ? objJSON.code_user = req.body.code_user : objJSON.code_user = 0;
-    req.body.code_session ? objJSON.code_session = req.body.code_session : objJSON.code_session = 0;
+    req.body.activate ? objJSON.activate = req.body.activate : objJSON.activate = 1;
+    req.body.full_name ? objJSON.full_name = req.body.full_name : objJSON.full_name = '';
     req.body.code_current ? objJSON.code_current = req.body.code_current : objJSON.code_current = await cod();
     req.body.code_relation ? objJSON.code_relation = req.body.code_relation : objJSON.code_relation = 0;
     req.body.code_before ? objJSON.code_before = req.body.code_before : objJSON.code_before = 0;
@@ -145,8 +213,9 @@ async function mountInsertObject(req) {
 async function mountJsonObjectUpdateDelete(req) {
     let objJSON = {};
     req.body.code_user ? objJSON.code_user = req.body.code_user : null;
+    req.body.activate ? objJSON.activate = req.body.activate : objJSON.activate = 1;
+    req.body.full_name ? objJSON.full_name = req.body.full_name : objJSON.full_name = '';
     req.body.code_current ? objJSON.code_current = req.body.code_current : null;
-    req.body.code_session ? objJSON.code_session = req.body.code_session : null;
     req.body.code_relation ? objJSON.code_relation = req.body.code_relation : null;
     req.body.code_before ? objJSON.code_before = req.body.code_before : null;
     req.body.input ? objJSON.input = req.body.input : null;
